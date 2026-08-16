8673111ea0a6f40001c70eb634769f8c0003fe2c
VHASH173 <mikeyesgeyv@gmail.com>
2026-08-16 00:57:48 -0500

    Fix: Fully migrate to bcryptjs and update lockfile to resolve ELF header error on Render

--- TransityDB.js
+++ TransityDB.js
@@ -1,6 +1,6 @@
 ﻿const express = require('express');
 const mysql = require('mysql2/promise');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
